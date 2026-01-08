7e5fc89eaad14587d8fecedb9f157c7338f6159e
Sunil Kumar <sunilkumar@sharviinfotech.com>
2026-01-08 12:36:08 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -29,3 +29,31 @@ const BASE_PORT = 3005
 app.listen(BASE_PORT, () =>
     console.log(`🚀 Server running on ${BASE_SERVER_URL}:${BASE_PORT}`)
 );
+
+// {
+//     const express = require('express');
+// const cors = require('cors');
+// const routes = require('./routes');
+// const connectDB = require('./config/db');
+
+// const app = express();
+
+// // CORS – allow nginx domain
+// app.use(cors({
+//     origin: ['http://103.69.28.102:8090'],
+//     credentials: true
+// }));
+// app.use(express.json({ limit: '50mb', strict: false }));
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api', routes);
+
+// connectDB();
+
+// // IMPORTANT: listen on 0.0.0.0
+// const PORT = 3009;
+// app.listen(PORT, '0.0.0.0', () => {
+//     console.log(`?? Backend running on port ${PORT}`);
+// });
+
+// }
